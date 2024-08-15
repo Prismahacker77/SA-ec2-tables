73c78c1f6e72c2453a57ac58348f98093f38e018
Nick O <noneil@Nicks-MacBook-Pro.local>
2024-08-15 16:06:52 -0400

    added route table gateway name

--- scan_ec2_instances_table.py
+++ scan_ec2_instances_table.py
@@ -5,20 +5,7 @@ def get_regions():
     ec2_client = boto3.client('ec2')
     return [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
 
-def get_public_subnets(ec2, vpc_id):
-    public_subnets = set()
-    route_tables = ec2.describe_route_tables(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])['RouteTables']
-
-    for rt in route_tables:
-        for route in rt['Routes']:
-            if route.get('DestinationCidrBlock') == '0.0.0.0/0' and route.get('GatewayId', '').startswith('igw-'):
-                for assoc in rt['Associations']:
-                    if assoc.get('SubnetId'):
-                        public_subnets.add(assoc['SubnetId'])
-
-    return public_subnets
-
-def get_gateway_for_subnet(ec2, subnet_id, vpc_id):
+def get_route_table_target(ec2, subnet_id, vpc_id):
     route_tables = ec2.describe_route_tables(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])['RouteTables']
     for rt in route_tables:
         for route in rt['Routes']:
@@ -44,7 +31,7 @@ def get_instances(ec2, region):
 def scan_ec2_instances():
     regions = get_regions()
     table = PrettyTable()
-    table.field_names = ["Region", "EC2 Count", "Instance ID", "Internet Accessible", "Public IP", "Subnet ID", "Gateway", "VPC ID", "Availability Zone"]
+    table.field_names = ["Region", "EC2 Count", "Instance ID", "Internet Accessible", "Public IP", "Subnet ID", "Route Table Target", "VPC ID", "Availability Zone"]
 
     for region in regions:
         ec2 = boto3.client('ec2', region_name=region)
@@ -58,13 +45,13 @@ def scan_ec2_instances():
                 subnet_id = instance.get('SubnetId')
                 public_ip = instance.get('PublicIpAddress')
                 az = instance.get('Placement', {}).get('AvailabilityZone', 'N/A')
-                gateway = get_gateway_for_subnet(ec2, subnet_id, vpc_id)
+                route_table_target = get_route_table_target(ec2, subnet_id, vpc_id)
                 internet_accessible = "No"
 
                 if vpc_id and subnet_id:
                     internet_accessible = is_internet_accessible(ec2, subnet_id, vpc_id)
 
-                table.add_row([region, ec2_count, instance_id, internet_accessible, public_ip, subnet_id, gateway, vpc_id, az])
+                table.add_row([region, ec2_count, instance_id, internet_accessible, public_ip, subnet_id, route_table_target, vpc_id, az])
 
     print(table)
 
