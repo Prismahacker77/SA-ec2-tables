bb23f3844350b1f44c2833ff54cca6cbaab75b5b
Nick O <noneil@Nicks-MacBook-Pro.local>
2024-08-15 16:25:34 -0400

    added new route table logic

--- scan_ec2_instances_table.py
+++ scan_ec2_instances_table.py
@@ -5,18 +5,25 @@ def get_regions():
     ec2_client = boto3.client('ec2')
     return [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
 
-def get_route_table_target_and_accessibility(ec2, subnet_id):
-    route_tables = ec2.describe_route_tables(Filters=[{'Name': 'association.subnet-id', 'Values': [subnet_id]}])['RouteTables']
-    
+def get_route_table_targets(ec2, vpc_id):
+    route_tables = ec2.describe_route_tables(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])['RouteTables']
+    subnet_to_target = {}
+
     for rt in route_tables:
         for route in rt['Routes']:
             if route.get('DestinationCidrBlock') == '0.0.0.0/0':
                 target = route.get('GatewayId') or route.get('NatGatewayId') or route.get('TransitGatewayId') or route.get('VpcPeeringConnectionId') or 'None'
+                target_name = 'None'
                 if target.startswith('igw-'):
-                    return target, "Yes"
-                else:
-                    return target, "No"
-    return 'None', 'No'
+                    target_name = 'IGW'
+                elif target.startswith('nat-'):
+                    target_name = 'NATGW'
+                
+                for assoc in rt['Associations']:
+                    if assoc.get('SubnetId'):
+                        subnet_to_target[assoc['SubnetId']] = target_name
+
+    return subnet_to_target
 
 def get_instances(ec2, region):
     return ec2.describe_instances()['Reservations']
@@ -37,7 +44,11 @@ def scan_ec2_instances():
                 subnet_id = instance.get('SubnetId')
                 public_ip = instance.get('PublicIpAddress')
                 az = instance.get('Placement', {}).get('AvailabilityZone', 'N/A')
-                route_table_target, internet_accessible = get_route_table_target_and_accessibility(ec2, subnet_id)
+
+                # Get the route table targets for the VPC
+                route_table_targets = get_route_table_targets(ec2, vpc_id)
+                route_table_target = route_table_targets.get(subnet_id, 'None')
+                internet_accessible = 'Yes' if route_table_target == 'IGW' else 'No'
 
                 table.add_row([region, instance_id, public_ip, subnet_id, vpc_id, az, route_table_target, internet_accessible])
 
