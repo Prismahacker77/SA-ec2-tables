7413fd8aed0bc7033728635786dd53f283a9184b
Nick O <noneil@Nicks-MacBook-Pro.local>
2024-08-15 16:16:41 -0400

    removed ec2 count and route table target

--- scan_ec2_instances_table.py
+++ scan_ec2_instances_table.py
@@ -5,19 +5,13 @@ def get_regions():
     ec2_client = boto3.client('ec2')
     return [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
 
-def get_route_table_target_and_accessibility(ec2, subnet_id):
-    # Describe route tables to find the one associated with the subnet
+def is_internet_accessible(ec2, subnet_id):
     route_tables = ec2.describe_route_tables(Filters=[{'Name': 'association.subnet-id', 'Values': [subnet_id]}])['RouteTables']
-    
     for rt in route_tables:
         for route in rt['Routes']:
-            if route.get('DestinationCidrBlock') == '0.0.0.0/0':
-                target = route.get('GatewayId') or route.get('NatGatewayId') or route.get('TransitGatewayId') or route.get('VpcPeeringConnectionId') or 'None'
-                if target.startswith('igw-'):
-                    return target, "Yes"
-                else:
-                    return target, "No"
-    return 'None', 'No'
+            if route.get('DestinationCidrBlock') == '0.0.0.0/0' and route.get('GatewayId', '').startswith('igw-'):
+                return "Yes"
+    return "No"
 
 def get_instances(ec2, region):
     return ec2.describe_instances()['Reservations']
@@ -25,12 +19,11 @@ def get_instances(ec2, region):
 def scan_ec2_instances():
     regions = get_regions()
     table = PrettyTable()
-    table.field_names = ["Region", "EC2 Count", "Instance ID", "Internet Accessible", "Public IP", "Subnet ID", "Route Table Target", "VPC ID", "Availability Zone"]
+    table.field_names = ["Region", "Instance ID", "Internet Accessible", "Public IP", "Subnet ID", "VPC ID", "Availability Zone"]
 
     for region in regions:
         ec2 = boto3.client('ec2', region_name=region)
         instances = get_instances(ec2, region)
-        ec2_count = sum(len(reservation['Instances']) for reservation in instances)
 
         for reservation in instances:
             for instance in reservation['Instances']:
@@ -39,9 +32,9 @@ def scan_ec2_instances():
                 subnet_id = instance.get('SubnetId')
                 public_ip = instance.get('PublicIpAddress')
                 az = instance.get('Placement', {}).get('AvailabilityZone', 'N/A')
-                route_table_target, internet_accessible = get_route_table_target_and_accessibility(ec2, subnet_id)
+                internet_accessible = is_internet_accessible(ec2, subnet_id)
 
-                table.add_row([region, ec2_count, instance_id, internet_accessible, public_ip, subnet_id, route_table_target, vpc_id, az])
+                table.add_row([region, instance_id, internet_accessible, public_ip, subnet_id, vpc_id, az])
 
     print(table)
 
