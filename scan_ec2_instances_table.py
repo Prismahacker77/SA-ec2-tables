5a4cbc3522450e89554e96c8a529f25ddec4c246
Nick O <noneil@Nicks-MacBook-Pro.local>
2024-08-15 15:50:11 -0400

    Updated code

---
+++ scan_ec2_instances_table.py
@@ -0,0 +1,52 @@
+import boto3
+from prettytable import PrettyTable
+
+def get_regions():
+    ec2_client = boto3.client('ec2')
+    return [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
+
+def get_public_subnets(ec2, vpc_id):
+    public_subnets = set()
+    route_tables = ec2.describe_route_tables(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])['RouteTables']
+
+    for rt in route_tables:
+        for route in rt['Routes']:
+            if route.get('DestinationCidrBlock') == '0.0.0.0/0' and route.get('GatewayId', '').startswith('igw-'):
+                for assoc in rt['Associations']:
+                    if assoc.get('SubnetId'):
+                        public_subnets.add(assoc['SubnetId'])
+
+    return public_subnets
+
+def get_instances(ec2, region):
+    return ec2.describe_instances()['Reservations']
+
+def scan_ec2_instances():
+    regions = get_regions()
+    table = PrettyTable()
+    table.field_names = ["Region", "EC2 Count", "Instance ID", "Internet Accessible", "Public IP", "Subnet ID", "VPC ID"]
+
+    for region in regions:
+        ec2 = boto3.client('ec2', region_name=region)
+        instances = get_instances(ec2, region)
+        ec2_count = sum(len(reservation['Instances']) for reservation in instances)
+
+        for reservation in instances:
+            for instance in reservation['Instances']:
+                instance_id = instance['InstanceId']
+                vpc_id = instance.get('VpcId')
+                subnet_id = instance.get('SubnetId')
+                public_ip = instance.get('PublicIpAddress')
+                internet_accessible = "No"
+
+                if vpc_id and subnet_id:
+                    public_subnets = get_public_subnets(ec2, vpc_id)
+                    if subnet_id in public_subnets and public_ip:
+                        internet_accessible = "Yes"
+
+                table.add_row([region, ec2_count, instance_id, internet_accessible, public_ip, subnet_id, vpc_id])
+
+    print(table)
+
+if __name__ == "__main__":
+    scan_ec2_instances()
